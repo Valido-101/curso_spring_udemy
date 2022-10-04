94a8505e0541e18510f0f2ec3a552f2ea1020fed
Valido-101 <jesuszafra22@gmail.com>
2022-10-05 00:46:48 +0200

    Ejercicio 1 completado

---
+++ curso_spring_udemy/src/com/jvalido/springdemo/FootballCoach.java
@@ -0,0 +1,10 @@
+package com.jvalido.springdemo;
+
+public class FootballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Párteles la madre a tus compañeros, perro";
+	}
+
+}
