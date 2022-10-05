0b3c8ed5db6dbbb2e18063113313d00a3902013e
Valido-101 <jesuszafra22@gmail.com>
2022-10-05 19:58:59 +0200

    actividad 2 realizada y funcional

--- curso_spring_udemy/src/com/jvalido/springdemo/HappyFortuneService.java
+++ curso_spring_udemy/src/com/jvalido/springdemo/HappyFortuneService.java
@@ -1,10 +1,16 @@
 package com.jvalido.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
 
 	@Override
 	public String getFortune() {
-		return "Today is your lucky day!";
+		//Generamos número aleatorio del 1 al 3
+		Random random = new Random();
+		
+		//Devolvemos una posición aleatoria del array entre 0 y 2
+		return fortunes[random.nextInt(3)];
 	}
 
 }
