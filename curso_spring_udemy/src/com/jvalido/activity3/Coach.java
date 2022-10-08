67a1b4d5d75b0d03816564e919dc2a7b18365138
Valido-101 <jesuszafra22@gmail.com>
2022-10-08 19:42:15 +0200

    Ejercicio 3 resuelto

---
+++ curso_spring_udemy/src/com/jvalido/activity3/Coach.java
@@ -0,0 +1,9 @@
+package com.jvalido.activity3;
+
+public interface Coach {
+	
+	public String getDailyWorkout();
+	
+	public String getDailyFortune();
+
+}
---
+++ curso_spring_udemy/src/com/jvalido/activity3/FortuneService.java
@@ -0,0 +1,7 @@
+package com.jvalido.activity3;
+
+public interface FortuneService {
+	
+	public String getFortune();
+	
+}
