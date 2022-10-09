701e949e0988710752bfe4657f6205bacb6df309
Valido-101 <jesuszafra22@gmail.com>
2022-10-09 20:38:38 +0200

    Configuracion de spring con archivo java realizado

---
+++ curso_spring_annotations_udemy/src/com/jvalido/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.jvalido.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.jvalido.springdemo")
+public class SportConfig {
+
+}
