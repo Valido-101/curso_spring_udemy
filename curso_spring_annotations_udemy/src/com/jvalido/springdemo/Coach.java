8a28537f64a5330b0f08b008d2ea033cdf218c53
Valido-101 <jesuszafra22@gmail.com>
2022-10-09 18:24:28 +0200

    Autowiring en constructor realizado

--- curso_spring_annotations_udemy/src/com/jvalido/springdemo/Coach.java
+++ curso_spring_annotations_udemy/src/com/jvalido/springdemo/Coach.java
@@ -3,5 +3,7 @@ package com.jvalido.springdemo;
 interface Coach {
 	
 	public String getDailyWorkout();
+	
+	public String getFortune();
 
 }
--- curso_spring_annotations_udemy/src/com/jvalido/springdemo/TennisCoach.java
+++ curso_spring_annotations_udemy/src/com/jvalido/springdemo/TennisCoach.java
@@ -1,5 +1,6 @@
 package com.jvalido.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 //Si indicamos un id junto con la anotación @Component, spring usará ese id
@@ -8,9 +9,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	//Esta anotación escanea el paquete hasta que encuentre una clase que implementa la interfaz del parámetro para añadirla al constructor
+	@Autowired
+	public TennisCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
