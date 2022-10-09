4ca47acbd316014fc080b387389c074c7cac5636
Valido-101 <jesuszafra22@gmail.com>
2022-10-09 18:29:29 +0200

    Setter injection con autowiring realizado en TennisCoach

--- curso_spring_annotations_udemy/src/com/jvalido/springdemo/TennisCoach.java
+++ curso_spring_annotations_udemy/src/com/jvalido/springdemo/TennisCoach.java
@@ -11,11 +11,25 @@ public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	//define default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	//define setter method
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println(">> TennisCoach: inside setFortune() method");
+		this.fortuneService = fortuneService;
+	}
+	
+	/*
 	//Esta anotación escanea el paquete hasta que encuentre una clase que implementa la interfaz del parámetro para añadirla al constructor
 	@Autowired
 	public TennisCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
+	*/
 	
 	@Override
 	public String getDailyWorkout() {
